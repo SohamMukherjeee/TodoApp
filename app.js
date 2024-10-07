4ef6553bc64cd212116e2ecbcbc143e92f46931e
Soham Mukherjee <myselfmukherjeesoham@gmail.com>
2024-10-08 02:08:06 +0530

    again

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require('path');  // Include path module
 require('dotenv').config();
 
 const app = express();
 let workList = [];
 
+// Set the view engine and the views directory
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));  // Specify the views directory
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('public')); // Static files location
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URL = process.env.MONGODB_URL;
